5d936addeeb775bbc785702896ae6f1b21c947a9
Alexander Guz <kalimatas@gmail.com>
2018-09-17 21:17:59 +0200

    CompletableFutures

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,9 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.ForkJoinTask;
 
@@ -16,20 +19,11 @@ public class App {
 
     private static HttpRequestFactory factory = new NetHttpTransport().createRequestFactory();
 
-    public static void main(String[] args) {
-        var urls = Arrays.asList("https://google.com/?q=sample",
-                                 "https://google.com/?q=sample",
-                                 "https://google.com/?q=sample",
-                                 "https://google.com/?q=sample",
-                                 "https://google.com/?q=sample",
-                                 "https://google.com/?q=sample",
-                                 "https://google.com/?q=sample",
-                                 "https://google.com/?q=sample",
-                                 "https://google.com/?q=sample",
-                                 "https://google.com/?q=sample",
-                                 "https://google.com/?q=sample");
+    private static final ExecutorService es = Executors.newFixedThreadPool(20);
 
-        parallelStreamCustomPool(urls);
+    public static void main(String[] args) {
+        final var urls = Collections.nCopies(10, "https://google.com/?q=sample");
+        futures(urls);
     }
 
     private static void sequentially(List<String> urls) {
@@ -69,6 +63,16 @@ public class App {
             e.printStackTrace();
         }
         logger.debug("Done fetching parallelStream with custom pool");
+    }
+
+    private static void futures(List<String> urls) {
+        ExecutorService es = Executors.newFixedThreadPool(10);
+        for (var url : urls) {
+            CompletableFuture.supplyAsync(() -> makeRequest(url), es)
+                             .thenAccept(res -> {
+                                 logger.debug("Got response of length {} from makeRequest", res.length());
+                             });
+        }
     }
 
     private static String makeRequest(String u) {
